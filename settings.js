9f9457549136c7877da7ab3de1fddcc6783f3f49
Ruskher <133194548+Ruskher@users.noreply.github.com>
2025-05-24 17:37:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Bot Si Ganteng',
   author: '‎',
-  botName: "Boos Bot",
+  botName: "Bot Si Ganteng",
   botOwner: 'Professor', // Your name
   ownerNumber: '6283890107168', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
