911b80884cba58c9c54e3982c763a9131ebadfb3
Ruskher <133194548+Ruskher@users.noreply.github.com>
2025-05-24 17:36:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Bot Si Ganteng',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Boos Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '6283890107168', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
